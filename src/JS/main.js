8ff439b3db9497ee642a7d43e6985ecccf13fe46
l3onardodev.github.io <leonardo.contato03@gmail.com>
2021-09-17 09:20:59 -0300

    Improvements in the UI; localStorage feature added; Responsivity for mobile and tablets added

--- src/JS/main.js
+++ src/JS/main.js
@@ -5,13 +5,26 @@ const modal = document.querySelector('.modal');
 const modalCancel = document.querySelector('.button-cancel-modal');
 const modalCreate = document.querySelector('.button-confirm-modal');
 const modalLabelError = document.querySelector('.modal-label-error');
+const modalBackground = document.querySelector('.modal-background');
 
 const taskContainer = document.querySelector('.tasks-container');
 const taskNameInput = document.querySelector('[data-js="task-name-input"]');
+const appContainer = document.querySelector('.flex-container');
 
+const screenHeight = screen.height;
+
+//se hovuer alguma task no localStorage, as mostra.
+getLocalStorageItems();
+
+//verifies height
+verifyHeight();
+
+
+//button to create a new task (opens the modal)
 newTaskButton.addEventListener('click', function() {
     modal.style.display = 'flex';
     modal.style.animation = 'modalAnimation 1s ease';
+    modalBackground.classList.remove('hidden');
 
     taskNameInput.focus();
 
@@ -20,11 +33,18 @@ newTaskButton.addEventListener('click', function() {
 
 clearAllButton.addEventListener('click', function() {
     taskContainer.innerHTML = '';
+    clearAllButton.classList.add('hidden');
+
+    //delete all the data in the localStorage
+    localStorage.clear();
+
+    verifyHeight();
 })
 
 modalCancel.addEventListener('click', function() {
     modal.style.display = 'none';
     modalLabelError.style.display = 'none';
+    modalBackground.classList.add('hidden');
 });
 
 document.addEventListener('keydown', function(event) {
@@ -35,6 +55,8 @@ document.addEventListener('keydown', function(event) {
         } else if (event.key === 'Escape') {
             modal.style.display = 'none';
             modalLabelError.style.display = 'none';
+            modalBackground.classList.add('hidden');
+            // modal.classList.add('hidden');
         }
     }
 });
@@ -42,38 +64,83 @@ document.addEventListener('keydown', function(event) {
 modalCreate.addEventListener('click', createItem);
 
 function createItem() {
+    verifyHeight();
+
+    // console.log(`total height:` + screen.height)
     if(taskNameInput.value.length === 0) {
         modalLabelError.style.display = 'block';
     } 
     else {
+        //armazena a task no localStorage
+        localStorage.setItem(taskNameInput.value, ' ');
+
+        //display the tasks accordingly the data coming from localStorage
+        getLocalStorageItems();
+
         modalLabelError.style.display = 'none';
         modal.style.display = 'none';
+        modalBackground.classList.add('hidden');
 
-        clearAllButton.classList.remove('hidden');
-
-        taskContainer.insertAdjacentHTML('afterbegin', `
-        <div class="task-item">
-            <div class="task-item-text">
-                <label>${taskNameInput.value}</label>
-            </div>
-            <div class="task-item-icons">
-                <img src="src/images/icon-done.png" data-js="done-icon">
-                <img src="src/images/icon-remove.png" data-js="remove-icon">
-            </div>
-        </div>
-        `)
+        if(clearAllButton.classList.contains('hidden')) clearAllButton.classList.remove('hidden');
         
-        b();
+        listenersDoneAndDeleteInTaskItem();
     }
 }
 
-function b() {
+function getLocalStorageItems() {
+    taskContainer.innerHTML = '';
+
+    if (localStorage.length === 0) {
+        return;
+    } else {
+        //display the clearAll button
+        clearAllButton.classList.remove('hidden');
+
+        const localStorageItems = Object.entries(localStorage);
+        console.log(localStorage);
+
+        localStorageItems.forEach((element) => {
+            taskContainer.insertAdjacentHTML('afterbegin', `
+            <div class="task-item">
+                <div class="task-item-text">
+                    <label>${element[0]}</label>
+                </div>
+                <div class="task-item-icons">
+                    <img src="src/images/icon-done.png" data-js="done-icon">
+                    <img src="src/images/icon-remove.png" data-js="remove-icon">
+                </div>
+            </div>`);
+        })
+        //add listeners to container task's buttons
+        listenersDoneAndDeleteInTaskItem();
+    }
+}
+
+function listenersDoneAndDeleteInTaskItem() {
+    console.log('cheguei aqui');
     document.querySelector('[data-js="remove-icon"]').addEventListener('click', function(event) {
         //remove item from list
-        event.target.parentElement.parentElement.remove();
+        const taskName = event.target.parentElement.parentElement.children[0].innerText;
+
+        localStorage.removeItem(taskName, ' ');
+        console.log('Excluído com sucesso.');
+        //get the data again and update the UI.
+        getLocalStorageItems();
     })
 
     document.querySelector('[data-js="done-icon"]').addEventListener('click', function(event) {
         event.target.parentElement.parentElement.firstElementChild.childNodes[1].style.textDecoration = 'line-through';
     })
+};
+
+function verifyHeight() {
+    console.log(appContainer.offsetHeight);
+    console.log(window.innerHeight);
+    if (appContainer.clientHeight >= window.innerHeight * 90 / 100) {
+        document.querySelector('.background').style.height = 'auto';
+        console.log('auto');
+    } else {
+        document.querySelector('.background').style.height = '100vh';
+        console.log('100vh');
+    }
 }
